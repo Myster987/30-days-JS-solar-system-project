f26a0f117e2ad19abe048d85a9f46d53c4c40388
Maciek <myster123987@gmail.com>
2023-12-06 22:21:43 +0100

    fixed error

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const calculateMass = (m, g) => `${Math.round(m * g)} N`;
 
 const gConstMap = new Map([
     ["Earth", 9.81],
-    ["Jupiter", 23,1],
+    ["Jupiter", 23, 1],
     ["Mars", 3.7],
     ["Moon", 1.622],
     ["Neptune", 11],
     ["Saturn", 9],
     ["Uranus", 8.7],
     ["Venus", 8.9],
-    ["Sun", 273.95]
+    ["Sun", 273.95],
 ]);
 
 const selectPlanet = document.getElementById("selectPlanet");
@@ -37,10 +37,15 @@ const calculateBtn = document.getElementById("calculateBtn");
 const resultWeigth = document.querySelector(".result-weigth");
 
 calculateBtn.onclick = () => {
-    const mass = Number((massInput.value != "") ? massInput.value : "not a number");
+    const mass = Number(massInput.value != "" ? massInput.value : "not a number");
     if (isNaN(mass)) {
         alert("Enter only numbers!");
-        return
+        return;
+    }
+
+    if (selectPlanet.value == "select object") {
+        alert("Select an object!");
+        return;
     }
 
     pathToImg = `./images/${selectPlanet.value}.png`;
@@ -54,8 +59,13 @@ calculateBtn.onclick = () => {
     resultContainer.style.gap = "0.5rem";
 
     const result = `<div class="final-result">
-                        <p>The weigth of this object on <strong>${selectPlanet.value}</strong></p>
-                        <div class="round-result">${calculateMass(mass, gConstMap.get(selectPlanet.value))}</div>
+                        <p>The weigth of this object on <strong>${
+                            selectPlanet.value
+                        }</strong></p>
+                        <div class="round-result">${calculateMass(
+                            mass,
+                            gConstMap.get(selectPlanet.value)
+                        )}</div>
                     </div>`;
 
     resultWeigth.innerHTML = result;
